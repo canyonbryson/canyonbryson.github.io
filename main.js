5fd5ac6d4fec00b778ab971af42be09cacab2a36
canyonbryson <58871744+canyonbryson@users.noreply.github.com>
2020-03-07 20:21:10 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function checkTime(i) {return (i < 10) ? "0" + i : i;}
+
+function startTime() {
+	var today = new Date(),
+	    h = checkTime(today.getHours()),
+	    m = checkTime(today.getMinutes()),
+	    s = checkTime(today.getSeconds());
+	document.getElementById('time').innerHTML = "Current Time, using Javascript: " + h + ":" + m + ":" + s;	
+}
+setInterval(startTime, 1000);
